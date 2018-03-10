9024a7e0513f87e181d992718e42ee75577df195
jonyroy <jonyroy16@gmail.com>
2018-03-10 20:52:27 +0600

    buble sort algorithm implementation in c by Jony Roy

---
+++ sorting/bubleSort/bubleSort.c
@@ -0,0 +1,35 @@
+/*
+
+Author: Jony Roy
+Email: jonyroyice@gmail.com
+Date: 10-03-2018
+
+Description:
+    Buble Sort Algorithm Implementation in C
+
+*/
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int n,m[100],i,tem,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    scanf("%d",&m[i]);
+    for(i=1;i<n;i++)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            if(m[j]>m[j+1])
+            {
+                temp=m[j];
+                m[j]=m[j+1];
+                m[j+1]=temp;
+            }
+        }
+    }
+    for(i=n;i>=1;i--)
+    printf("%d ",m[i]);
+    return 0;
+}
